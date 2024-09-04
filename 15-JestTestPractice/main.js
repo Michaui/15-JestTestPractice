b7761d43be22dc35a7a7ea99c0c43b6611bbe2f3
Michaui <michael_franiczek@web.de>
2024-09-04 21:06:08 +0200

    refresh

---
+++ 15-JestTestPractice/main.js
@@ -0,0 +1,18 @@
+import './style.css'
+
+/*
+export function toUpperCase(string){
+  let arrayString = []; 
+  arrayString = string.split(''); //Mache ein Array aus allen Zeichen
+  let b = arrayString.splice(0,1).toString().toUpperCase(); //Nehme die erste Position, wandle es zum String und setzte es zum Uppaercase. 
+  return b + arrayString.join(''); //Füge UpperCase Buchstabe hinzu und mache das Array zum String. 
+};
+*/
+
+export function capitalize(string) {
+  if (typeof string !== 'string') return ''; //Sicherheitsüberprüfung, um Edge Case einzugerenzen. 
+  return string.charAt(0).toUpperCase() + string.slice(1); //(1.) Nimmt das erste Char und wandelt es um -> (2.) fügt es dem zweiten String hinzu, wo das erste Char entfernt wurde. 
+}
+
+console.log(toUpperCase('hallo')); 
+console.log(capitalize('hallo')); 
