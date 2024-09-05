f9b64fc51b845a019bb6f7defada04360d1c40eb
Michaui <michael_franiczek@web.de>
2024-09-05 08:19:18 +0200

    refresh

--- function.js
+++ function.js
@@ -9,4 +9,4 @@
 export function capitalize(string) {
     if (typeof string !== 'string') return ''; // Checks if input is a string
     return string.charAt(0).toUpperCase() + string.slice(1); // Capitalizes the first character and appends the rest of the string
-  }
+}
